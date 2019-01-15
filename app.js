c150876fbf8a55592430faef6de7fb994221805f
liw <liw>
2019-01-15 11:45:59 +0800

    computed

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 new Vue({
     el: "#canvas",
     data: {
-        X: 0,
-        Y: 0,
+        num1: 0,
+        num2: 0,
+        age: 20
     },
     methods: {
-        updateXY: function (event) {
-            this.X = event.offsetX;
-            this.Y = event.offsetY;
-        }
+        // addToA: function (params) {
+        //     console.log('add to a')
+        //     return this.num1 + this.age;
+        // },
+        // addToB: function (params) {
+        //     console.log('add to b')
+        //     return this.num2 + this.age;
+        // },
+    },
+    computed: {
+        addToA: function (params) {
+            console.log('add to a')
+            return this.num1 + this.age;
+        },
+        addToB: function (params) {
+            console.log('add to b')
+            return this.num2 + this.age;
+        },
     },
 })
