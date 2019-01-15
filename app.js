da2a7433c4b743e71b499c3d4713571802a60c77
liw <liw>
2019-01-15 11:50:24 +0800

    class

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 new Vue({
     el: "#canvas",
     data: {
-        num1: 0,
-        num2: 0,
-        age: 20
+        available: false,
+        nearby: false
     },
     methods: {
-        // addToA: function (params) {
-        //     console.log('add to a')
-        //     return this.num1 + this.age;
-        // },
-        // addToB: function (params) {
-        //     console.log('add to b')
-        //     return this.num2 + this.age;
-        // },
+
     },
     computed: {
-        addToA: function (params) {
-            console.log('add to a')
-            return this.num1 + this.age;
-        },
-        addToB: function (params) {
-            console.log('add to b')
-            return this.num2 + this.age;
-        },
-    },
+        compClasses: function(){
+            return{
+                available: this.available,
+                nearby: this.nearby,
+            }
+        }
+    }
 })
