cbda8265769de35a68f86421bce9c53a42b518ae
liw <liw>
2019-01-17 12:46:23 +0800

    生命周期

--- vue-demo1/src/components/Header.vue
+++ vue-demo1/src/components/Header.vue
@@ -26,6 +26,37 @@ export default {
             this.$emit('titleChanged', "子向父传值")
         }
     },
+    // 可用以设置加载动画
+    beforeCreate() {
+        alert("组件实例化之前执行的函数")
+    },
+    //请求接口/结束加载
+    created() {
+        alert("组件实例化之前完毕，但页面还未显示的")
+    },
+    //检查el或vue.$mount(el)->检查template
+    //挂载前
+    beforeMount() {
+        alert("组件挂载前，页面仍未展示，但虚拟dom已经配置")
+    },
+    //挂载，方法结束后页面显示
+    mounted() {
+        alert("组件挂载后，此方法执行后，页面显示")
+    },
+    //组件更新前
+    beforeUpdate() {
+        alert("组件更新前，页面仍未更新，但虚拟dom已经配置")
+    },
+    updated() {
+        alert("组件更新后，此方法执行后，页面显示")
+    },
+    beforeDestroy() {
+        alert("组件销毁前")
+    },
+    destroyed() {
+        alert("组件已经销毁")
+    },
+
 }
 
 </script>
