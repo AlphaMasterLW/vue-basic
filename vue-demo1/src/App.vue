ccd4b92a1c161f9bf04f0b2c34315afe272bd46b
liw <liw>
2019-01-17 15:58:26 +0800

    router和source

--- vue-demo1/src/App.vue
+++ vue-demo1/src/App.vue
@@ -1,51 +1,27 @@
 <template>
   <div id="app">
-    <app-header @titleChanged="updateTitle($event)" :titles="title"/>
-    <hello-world :len="users.length"></hello-world>
-    <users :users="users"/>
-    <app-footer :titles="title"/>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/helloworld">HelloWorld</router-link></li>
+    </ul>
+    <router-view></router-view>
   </div>
 </template>
 
-<!--
-  传值： string number boolean
-  传引用： array object
--->
-
 <script>
-import Users from './components/Users'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
   components: {
-    "users": Users,
-    "app-header": Header,
-    "app-footer": Footer,
-    "hello-world": HelloWorld
+
   },
   data() {
     return {
-      users: [
-                {name: "herry",position: "web开发", show: false},
-                {name: "bucky",position: "python开发", show: false},
-                {name: "emily",position: "java开发", show: false},
-                {name: "herry",position: "web开发", show: false},
-                {name: "bucky",position: "python开发", show: false},
-                {name: "emily",position: "java开发", show: false},
-                {name: "herry",position: "web开发", show: false},
-                {name: "bucky",position: "python开发", show: false},
-                {name: "emily",position: "java开发", show: false},
-            ],
-      title: '我是title'
+
     }
   },
   methods: {
-    updateTitle: function name(title) {
-      this.title = title;
-    }
+
   },
 }
 </script>
